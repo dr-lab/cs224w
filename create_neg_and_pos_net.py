79578850ed08d40efe9a334621f5221d9d391983
Sullivan, Danielle Stephanie <sullivan42@llnl.gov>
2018-12-08 13:13:14 -0800

    create code for creating pos and neg nets, ensuring no nodes lost

--- create_neg_and_pos_net.py
+++ create_neg_and_pos_net.py
@@ -7,7 +7,7 @@ csv files
 """
 
 import pandas as pd
-
+import networkx as nx
 network = "alpha"
 
 net = pd.read_csv("./rev2/data/%s_network.csv"%(network),header=None)
@@ -16,5 +16,27 @@ net_neg = net[net[2] < 0]
 net_pos = net[net[2] >= 0]
 net_pos.to_csv("%s_pos_network.csv"%(network),header=False)
 net_neg.to_csv("%s_neg_network.csv"%(network),header=False)
+
+def getPosNegNet(dataset):
+    G = nx.read_gpickle("results/%s_graph_embedding_featured_graph.pkl" % (dataset))
+    Gpos = nx.read_gpickle("results/%s_graph_embedding_featured_graph.pkl" % (dataset))
+    Gneg = nx.read_gpickle("results/%s_graph_embedding_featured_graph.pkl" % (dataset))
+
+    for e in G.edges_iter(data='weight', default=1):
+        if e[2] >= 0:
+            Gneg.remove_edge(e[0],e[1])
+        if e[2] < 0:
+            Gpos.remove_edge(e[0],e[1])  
+    # Error  checking
+    for e in Gpos.edges_iter(data='weight', default=1):
+        if e[2]<0:
+            raise ValueError('pos net has neg weight')
+        
+    for e in Gneg.edges_iter(data='weight', default=1):
+        if e[2]>0:
+            raise ValueError('neg net has pos weight')
+    return Gpos, Gneg
+
+Gpos,Gneg = getPosNegNet(network)
 
 
