0a8598d6fba93ca29f6eddb1af86ab75ba6d7d39
Sullivan, Danielle Stephanie <sullivan42@llnl.gov>
2018-12-09 12:46:15 -0800

    decision tree now runs through all embeddings and pringts score

--- decision_tree.py
+++ decision_tree.py
@@ -10,14 +10,14 @@ import pandas as pd
 import sklearn.tree
 from sklearn.model_selection import train_test_split
 import subprocess
-import export_graphviz
+#import export_graphviz
 network = "alpha"
-
+"""
 #nodeid is column 0
 gt = pd.read_csv("./rev2/data/%s_gt.csv"%(network),header=None)
 
 #nodeid=col 0
-features = pd.read_csv("./results/%s_graph_embedding_vectors.csv"%(network),header=None)
+features = pd.read_csv("./results/%s_graph_embedding_vectors_features.csv"%(network),header=None)
 
 out = gt.merge(features,'inner',left_on=0,right_on=0,suffixes=("_y","_x"))
 
@@ -54,3 +54,37 @@ net_neg.to_csv("%s_neg_network.csv"%(network),header=False)
 
 
 gt_net = gt.merge(net,'inner',left_on=0,right_on=0,suffixes=("_y","_x"))
+"""
+feats = ["features", "features_pos_neg", "features_pos_neg_rev2"]
+modes = ['cosine', 'l2']
+
+for f in feats:
+    gt = pd.read_csv("./rev2/data/%s_gt.csv"%(network),header=None)
+    
+    #nodeid=col 0
+    features = pd.read_csv("./results/%s_graph_embedding_vectors_%s.csv"%(network,f),header=None)
+    
+    out = gt.merge(features,'inner',left_on=0,right_on=0,suffixes=("_y","_x"))
+    
+    y = out["1_y"].copy()
+    X = out.drop('1_y', axis=1)
+    X = X.drop(0, axis=1)
+    
+    dt = sklearn.tree.DecisionTreeClassifier(min_samples_split=20, random_state=99)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+    dt.fit(X_train, y_train)
+    score = dt.score(X_test,y_test)
+    print "========="
+    print "Feature: %s" % f
+    print "score %f" % score
+    dt.decision_path(X_test)
+    
+    #export_graphviz(dt,out_file='dt.dot')
+    command = ["dot", "-Tpng", "dt.dot", "-o", "dt.png"]
+    subprocess.check_call(command)
+    
+        
+        
+        
+        
+        
