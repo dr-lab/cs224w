67f3964ea5bdeeaf177d4953672be3a25e4ab8b7
tmt <minhtuan.tran96@gmail.com>
2018-12-08 22:56:24 -0800

    updated k-means

--- k-means.py
+++ k-means.py
@@ -6,6 +6,11 @@ import random
 import networkx as nx
 import numpy as np
 import cPickle
+from sklearn.manifold import TSNE
+from matplotlib import pyplot as plt
+import hdbscan
+import seaborn as sns
+
 import ground_truth_analysis as gt
 
 dataset = "alpha"
@@ -20,6 +25,7 @@ def load_graph():
 
     return G
 
+
 def distance(x, y):
     return np.sqrt(np.sum(np.square(x - y)))
     # x2 = np.sqrt(np.sum(np.square(x)))
@@ -30,9 +36,9 @@ def distance(x, y):
 
     # return np.matmul(x, y)/ (x2 * y2)
 
+
 def find_closest_center(score, centers):
-    return min(
-        range(len(centers)), key=lambda i: distance(score, centers[i]))
+    return min(range(len(centers)), key=lambda i: distance(score, centers[i]))
 
 
 def k_means_clustering(score_map, k):
@@ -47,10 +53,11 @@ def k_means_clustering(score_map, k):
         }
 
         # 2b. Find new centers from means of points
-        new_centers = np.array([
-            np.array([score_map[j] for j in score_map
-                      if labels[j] == i]).mean(0) for i in range(k)
-        ])
+        new_centers = np.copy(centers)
+        for i in range(k):
+            cluster = [score_map[j] for j in score_map if labels[j] == i]
+            if len(cluster) != 0:
+                new_centers[i] = np.array(cluster).mean(0)
 
         # 2c. Check for convergence
         if np.all(centers == new_centers):
@@ -62,31 +69,67 @@ def k_means_clustering(score_map, k):
 
 def main():
     graph = load_graph()
-    score_map = {
-        node_id: node["features"][1:]
-        for node_id, node in graph.node.items()
-    }
+    feature_name = "features"
+    features = np.array(
+        [node[feature_name][1:] for node in graph.node.values()])
+    reduced_features = TSNE(n_components=2).fit_transform(features)
+    # plt.scatter(*reduced_features.T, s=50, linewidth=0, c='b', alpha=0.25)
+    # plt.show()
+
+    score_map = dict(zip(graph.node.keys(), reduced_features))
 
     gt_bad_users = cPickle.load(
         open("./results/%s_gt_bad_users_set.pkl" % dataset, "rb"))
 
-    gt_good_users = cPickle.load(	
-        open("./results/%s_gt_good_users_set.pkl" % dataset, "rb"))	
-    print "Number of ground truth bad users: %d " % len(gt_bad_users)	
-    print "Number of ground truth good users: %d" % len(gt_good_users)	
-    for k in range(2, 10):	
-        print	
-        print "For %d clusters" % k	
-        centers, labels = k_means_clustering(score_map, k)	
-        for i in range(k):	
-            cluster = [node_id for node_id in labels if labels[node_id] == i]	
-            print "Cluster ", i, " has: "	
-            print "%d users" % len(cluster)	
-            bad_users = set(gt_bad_users) & set(cluster)	
-            good_users = set(gt_good_users) & set(cluster)	
-            print "%d ground truth bad users, %.2f%% of the cluster" % (len(bad_users), len(bad_users) / len(cluster) * 100)	
-            print "%d ground truth good users, %.2f%% of the cluster" % (len(	
-                good_users), len(good_users) / len(cluster) * 100)
+    gt_good_users = cPickle.load(
+        open("./results/%s_gt_good_users_set.pkl" % dataset, "rb"))
+    print "Number of ground truth bad users: %d " % len(gt_bad_users)
+    print "Number of ground truth good users: %d" % len(gt_good_users)
+
+
+    clusterer = hdbscan.HDBSCAN(min_cluster_size=50).fit(reduced_features)
+    color_palette = sns.color_palette('deep', 20)
+    print clusterer.labels_.max()
+    cluster_colors = [
+        color_palette[x] if x >= 0 else (0.5, 0.5, 0.5) for x in clusterer.labels_
+    ]
+    cluster_member_colors = [
+        sns.desaturate(x, p)
+        for x, p in zip(cluster_colors, clusterer.probabilities_)
+    ]
+
+    plt.clf()
+    plt.scatter(*reduced_features.T, s=50, linewidth=0, c=cluster_member_colors, alpha=0.25)
+    plt.savefig('hdbscan.png')
+
+    centers, labels = k_means_clustering(score_map, 10)
+    cluster_colors = [
+        color_palette[labels[node_id]] if x >= 0 else (0.5, 0.5, 0.5) for node_id in labels
+    ]
+    plt.clf()
+    plt.scatter(
+        *reduced_features.T,
+        s=50,
+        linewidth=0,
+        c=cluster_colors,
+        alpha=0.25)
+    plt.savefig('k_means.png')
+
+    # for k in range(2, 10):
+    # print
+    # print "For %d clusters" % k
+    # centers, labels = k_means_clustering(score_map, k)
+    # for i in range(k):
+    #     cluster = [node_id for node_id in labels if labels[node_id] == i]
+    #     print "Cluster ", i, " has: "
+    #     print "%d users" % len(cluster)
+    #     bad_users = set(gt_bad_users) & set(cluster)
+    #     good_users = set(gt_good_users) & set(cluster)
+    #     print "%d ground truth bad users, %.2f%% of the cluster" % (
+    #         len(bad_users), len(bad_users) / len(cluster) * 100)
+    #     print "%d ground truth good users, %.2f%% of the cluster" % (
+    #         len(good_users), len(good_users) / len(cluster) * 100)
+
 
 if __name__ == "__main__":
     main()
